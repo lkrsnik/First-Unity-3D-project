c779e5db8224e25634ec9c687239799e81a4d1f7
Ozbolt Menegatti <ozbolt.menegatti@gmail.com>
2014-01-13 13:04:04 +0100

    igranje z elevatorjem

--- Igra/Assets/Elevator/Lift.js
+++ Igra/Assets/Elevator/Lift.js
@@ -1,45 +1,17 @@
 ﻿#pragma strict
 
-private var pressedButton : boolean = false;
 private var isElevatorUp : boolean = false;
-//private var TEST : boolean = false;
 
-var target : GameObject;
-
-function OnMouseOver()
+function startMoving()
 {
-	pressedButton = true;
-}
-
-function OnMouseExit()
-{
-	pressedButton = false;
-}
-
-function OnMouseDown()
-{
-	
 	if(isElevatorUp == false)
 	{
-		//TEST=true;
-		//target = GameObject.Find("Bla");
-		//target.renderer.enabled = false;
 		animation.Play("Up");
 		isElevatorUp = true;
 	}
 	else
 	{
-		target =GameObject.Find("Elevator");
-		target.animation.Play("Down");
+		animation.Play("Down");
 		isElevatorUp = false;
-	}
-}
-
-function OnGUI()
-{
-	//if(pressedButton == true && TEST)
-	if(pressedButton == true)
-	{
-		GUI.Box(new Rect(300,300,200,20), "Press to use lift");
 	}
 }
